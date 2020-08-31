ca2d33e31e926d23dbf06d20ee1e8ca550cf5240
LittleCitizen11 <thelittlecitizen@gmail.com>
2020-08-31 19:47:24 +0300

    add project

---
+++ HelloLog/HelloLog/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Serilog;
+
+namespace HelloLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger log = new LoggerConfiguration().WriteTo.File("log.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+            log.Information("Hello Serilog");
+        }
+    }
+}
