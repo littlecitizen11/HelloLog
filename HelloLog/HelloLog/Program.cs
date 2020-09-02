5059deafa03b2a69127283e549e3e67843f1b9b1
LittleCitizen11 <thelittlecitizen@gmail.com>
2020-09-02 09:22:37 +0300

    add serilog and appsettings

--- HelloLog/HelloLog/Program.cs
+++ HelloLog/HelloLog/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 
 namespace HelloLog
@@ -7,8 +10,23 @@ namespace HelloLog
     {
         static void Main(string[] args)
         {
-            ILogger log = new LoggerConfiguration().WriteTo.File("log.txt", rollingInterval: RollingInterval.Day).CreateLogger();
-            log.Information("Hello Serilog");
+            IConfiguration configSerilog = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json", true, true)
+               .Build();
+
+
+            ILogger log = new LoggerConfiguration()
+                 .ReadFrom.Configuration(configSerilog)
+                          .CreateLogger();
+
+
+            log.Information("Initializing Serilog....");
+
+
+
+      /*            ILogger logToConsole = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+                        *//*ILogger logToFile = new LoggerConfiguration().WriteTo.File("log.txt", rollOnFileSizeLimit:true, fileSizeLimitBytes:1024).CreateLogger();
+                        logToFile.Information("Hello Serilog");*/
         }
     }
 }
